0fc90a7ce3eb414654e0263493ed5eaeb952459c
Jnmendza <jonatmendoza@gmail.com>
2019-08-20 13:56:05 -0700

    callbacks-stuck on the last one

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,7 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+// /* 
 
   // GIVEN THIS PROBLEM:
 
@@ -27,6 +27,12 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   const test1 = firstItem(items, item => `I love my ${item}!`);
   console.log(test1); // "I love my Pencil!"
+  
+//   const test1 = firstItem(items, function (item){    <------------long version
+//     return "I love my ".concat(item, "!");
+// });
+console.log(test1);
+
 
   // TEST 2 (declaring callback before hand):
 
@@ -36,34 +42,67 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
-*/
+// */
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+
+getLength(items, function(arrLength){
+  console.log(arrLength);
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+last(items, function(lastItem){
+  console.log(lastItem)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+
+const add = (x,y) => {
+  return x + y;
+}
+//ALSO written this way
+// const add = function add(x,y) {
+//   return x + y;
+// };
+
+console.log(add(2,4));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y)
 }
+
+const multiply = (x,y) => {
+  return x * y;
+}
+// const multiply = function multiply(x,y){
+//   return x * y;
+// }
+
+console.log(multiply(3,4));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  
 }
 
-/* STRETCH PROBLEM */
+// /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
